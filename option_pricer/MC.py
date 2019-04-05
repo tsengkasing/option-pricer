02bbfb30e78517375609bfac91850e28e8460b31
YANDaoyu <yqy.yan@qq.com>
2019-04-05 11:07:47 +0800

    update MC, but have bugs in control variate

---
+++ option_pricer/MC.py
@@ -0,0 +1,231 @@
+'''
+created by @ Qiangyu YAN
+'''
+import closed_form_formulas as form
+import numpy as np
+from scipy.stats import norm
+import random as rd
+rd.seed(10)
+
+##########################################
+# Arith Call Option, return 2 number
+# as interval begin and end
+# m is the number of paths
+# control is bool, false - no control
+##########################################
+def Arith_Call_Option(S_0, sigma, r, T, K, n, m, control, seed):
+    Dt = T/n
+    geo = form.geom_asian_call_option(S_0, sigma, r, T, K, n, t=0)
+    np.random.seed(seed)
+    mu = np.exp((r - 0.5*sigma*sigma) * Dt)
+    arithPayoff, geoPayoff = [], []
+    for i in range(m):
+        growthFactor = mu * np.exp(sigma * np.sqrt(Dt) * np.random.standard_normal())
+        Spath = []
+        Spath.append(S_0 * growthFactor)
+        for j in range(n):
+            # from lecture 4, page 16
+            growthFactor = mu * np.exp(sigma * np.sqrt(Dt)*np.random.standard_normal())
+            Spath.append(Spath[-1] * growthFactor)
+        # Arithmetic mean
+        arithMean = np.mean(Spath)
+        arithPayoff.append(np.exp(-r*T) * max(arithMean - K, 0))
+        # Geometric mean
+        if(control):
+            geoMean = np.exp( (1/n) * np.sum(np.log(Spath)))
+            geoPayoff.append(np.exp(-r*T) * max(geoMean - K, 0))
+    if(control):
+        covXY = np.mean(np.multiply(arithPayoff,geoPayoff)) \
+            - np.mean(arithPayoff) * np.mean(geoPayoff)
+        theta = covXY / np.var(geoPayoff)
+        Z = arithPayoff + theta * (geo - geoPayoff)
+        Zmean = np.mean(Z)
+        Zstd = np.std(Z)
+        print(geo, Zmean, Zstd, np.mean(geoPayoff))
+        return Zmean-1.96*Zstd/np.sqrt(m), Zmean+1.96*Zstd/np.sqrt(m)
+    else:
+        Pmean = np.mean(arithPayoff)
+        Pstd = np.std(arithPayoff)
+        return Pmean-1.96*Pstd/np.sqrt(m), Pmean+1.96*Pstd/np.sqrt(m)
+
+
+
+##########################################
+# Arith Put Option, return 2 number
+# as interval begin and end
+# m is the number of paths
+# control is bool, false - no control
+##########################################
+def Arith_Put_Option(S_0, sigma, r, T, K, n, m, control, seed):
+    Dt = T/n
+    geo = form.geom_asian_put_option(S_0, sigma, r, T, K, n, t=0)
+    np.random.seed(seed)
+    mu = np.exp((r - 0.5*sigma*sigma) * Dt)
+    arithPayoff, geoPayoff = [], []
+    for i in range(m):
+        growthFactor = mu * np.exp(sigma * np.sqrt(Dt) * np.random.standard_normal())
+        Spath = []
+        Spath.append(S_0 * growthFactor)
+        for j in range(n):
+            # from lecture 4, page 16
+            growthFactor = mu * np.exp(sigma * np.sqrt(Dt)*np.random.standard_normal())
+            Spath.append(Spath[-1] * growthFactor)
+        # Arithmetic mean
+        arithMean = np.mean(Spath)
+        arithPayoff.append(np.exp(-r*T) * max(K - arithMean, 0))
+        # Geometric mean
+        if(control):
+            geoMean = np.exp( 1/n * np.sum(np.log(Spath)))
+            geoPayoff.append(np.exp(-r*T) * max(K - geoMean, 0))
+    if(control):
+        covXY = np.mean(np.multiply(arithPayoff,geoPayoff)) \
+            - np.mean(arithPayoff) * np.mean(geoPayoff)
+        theta = covXY / np.var(geoPayoff)
+        Z = arithPayoff + theta * (geo - geoPayoff)
+        Zmean = np.mean(Z)
+        Zstd = np.std(Z)
+        return Zmean-1.96*Zstd/np.sqrt(m), Zmean+1.96*Zstd/np.sqrt(m)
+    else:
+        Pmean = np.mean(arithPayoff)
+        Pstd = np.std(arithPayoff)
+        return Pmean-1.96*Pstd/np.sqrt(m), Pmean+1.96*Pstd/np.sqrt(m)
+
+
+
+
+##########################################
+# Arith Mean Call Basket, return 2 number
+# as interval begin and end
+# m is the number of paths
+# control is bool, false - no control
+##########################################
+def Arith_Call_Basket(S_0_1, S_0_2, sigma_1, sigma_2, r, T, K, rho, m, control, seed):
+    geo = form.geom_basket_call_option(S_0_1, S_0_2, sigma_1, sigma_2, r, T, K, rho, t=0)
+    print(geo)
+    np.random.seed(seed)
+    arithPayoff, geoPayoff = [], []
+    for i in range(m):
+        Z1 = np.random.standard_normal()
+        Z2 = rho*Z1 + np.sqrt(1 - rho*rho)*np.random.standard_normal()
+        S_1 = S_0_1 * np.exp( (r - 0.5*sigma_1*sigma_1)*T \
+            + sigma_1 * np.sqrt(T) * Z1 )
+        S_2 = S_0_2 * np.exp( (r - 0.5*sigma_2*sigma_2)*T \
+            + sigma_2 * np.sqrt(T) * Z2 )
+        Spath = [S_1, S_2]
+        # Arithmetic mean
+        arithMean = np.mean(Spath)
+        arithPayoff.append(np.exp(-r*T) * max(arithMean - K, 0))
+        # Geometric mean
+        if(control):
+            geoMean = np.exp( 0.5 * np.sum(np.log(Spath)))
+            geoPayoff.append(np.exp(-r*T) * max(geoMean - K, 0))
+    if(control):
+        covXY = np.mean(np.multiply(arithPayoff,geoPayoff)) \
+            - np.mean(arithPayoff) * np.mean(geoPayoff)
+        theta = covXY / np.var(geoPayoff)
+        Z = arithPayoff + theta * (geo - geoPayoff)
+        Zmean = np.mean(Z)
+        Zstd = np.std(Z)
+        return Zmean-1.96*Zstd/np.sqrt(m), Zmean+1.96*Zstd/np.sqrt(m)
+    else:
+        Pmean = np.mean(arithPayoff)
+        Pstd = np.std(arithPayoff)
+        return Pmean-1.96*Pstd/np.sqrt(m), Pmean+1.96*Pstd/np.sqrt(m)
+
+
+
+##########################################
+# Arith Mean Put Basket, return 2 number
+# as interval begin and end
+# m is the number of paths
+# control is bool, false - no control
+##########################################
+def Arith_Put_Basket(S_0_1, S_0_2, sigma_1, sigma_2, r, T, K, rho, m, control, seed):
+    geo = form.geom_basket_put_option(S_0_1, S_0_2, sigma_1, sigma_2, r, T, K, rho, t=0)
+    np.random.seed(seed)
+    arithPayoff, geoPayoff = [], []
+    for i in range(m):
+        Z1 = np.random.standard_normal()
+        Z2 = rho*Z1 + np.sqrt(1 - rho*rho)*np.random.standard_normal()
+        S_1 = S_0_1 * np.exp( (r - 0.5*sigma_1*sigma_1)*T \
+            + sigma_1 * np.sqrt(T) * Z1 )
+        S_2 = S_0_2 * np.exp( (r - 0.5*sigma_2*sigma_2)*T \
+            + sigma_2 * np.sqrt(T) * Z2 )
+        Spath = [S_1, S_2]
+        # Arithmetic mean
+        arithMean = np.mean(Spath)
+        arithPayoff.append(np.exp(-r*T) * max(K - arithMean, 0))
+        # Geometric mean
+        if(control):
+            geoMean = np.exp( 0.5 * np.sum(np.log(Spath)))
+            geoPayoff.append(np.exp(-r*T) * max(K - geoMean, 0))
+    if(control):
+        covXY = np.mean(np.multiply(arithPayoff,geoPayoff)) \
+            - np.mean(arithPayoff) * np.mean(geoPayoff)
+        theta = covXY / np.var(geoPayoff)
+        Z = arithPayoff + theta * (geo - geoPayoff)
+        Zmean = np.mean(Z)
+        Zstd = np.std(Z)
+        return Zmean-1.96*Zstd/np.sqrt(m), Zmean+1.96*Zstd/np.sqrt(m)
+    else:
+        Pmean = np.mean(arithPayoff)
+        Pstd = np.std(arithPayoff)
+        return Pmean-1.96*Pstd/np.sqrt(m), Pmean+1.96*Pstd/np.sqrt(m)
+
+
+r = 0.05
+T = 3
+S = 100
+m = 100000
+
+# Arith_Call_Option(S_0, sigma, r, T, K, n, m, control, seed):
+# print( Arith_Call_Option(S, 0.3, r, T, 100, 50, m, False, 10) )
+# print( Arith_Call_Option(S, 0.3, r, T, 100, 100, m, False, 10) )
+# print( Arith_Call_Option(S, 0.4, r, T, 100, 50, m, False, 10) )
+
+# print( Arith_Call_Option(S, 0.3, r, T, 100, 50, m, True, 10) )
+# print( Arith_Call_Option(S, 0.3, r, T, 100, 100, m, True, 10) )
+# print( Arith_Call_Option(S, 0.4, r, T, 100, 50, m, True, 10) )
+
+
+# # # Arith_Put_Option(S_0, sigma, r, T, K, n, m, control, seed):
+# print( Arith_Put_Option(S, 0.3, r, T, 100, 50, m, False, 10) )
+# print( Arith_Put_Option(S, 0.3, r, T, 100, 100, m, False, 10) )
+# print( Arith_Put_Option(S, 0.4, r, T, 100, 50, m, False, 10) )
+
+# print( Arith_Put_Option(S, 0.3, r, T, 100, 50, m, True, 10) )
+# print( Arith_Put_Option(S, 0.3, r, T, 100, 100, m, True, 10) )
+# print( Arith_Put_Option(S, 0.4, r, T, 100, 50, m, True, 10) )
+
+
+# # Arith_Call_Basket(S_0_1, S_0_2, sigma_1, sigma_2, 
+# #                   r, T, K, rho, m, control, seed)
+print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 100, 0.5, m, False, 10))
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 100, 0.9, m, False, 10))
+# print(Arith_Call_Basket(S, S, 0.1, 0.3, r, T, 100, 0.5, m, False, 10))
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 80, 0.5, m, False, 10))
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 120, 0.5, m, False, 10))
+# print(Arith_Call_Basket(S, S, 0.5, 0.5, r, T, 100, 0.5, m, False, 10))
+
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 100, 0.5, m, True, 10))
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 100, 0.9, m, True, 10))
+# print(Arith_Call_Basket(S, S, 0.1, 0.3, r, T, 100, 0.5, m, True, 10))
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 80, 0.5, m, True, 10))
+# print(Arith_Call_Basket(S, S, 0.3, 0.3, r, T, 120, 0.5, m, True, 10))
+# print(Arith_Call_Basket(S, S, 0.5, 0.5, r, T, 100, 0.5, m, True, 10))
+
+# # Arith_Call_Basket(S_0_1, S_0_2, sigma_1, sigma_2, 
+# #                   r, T, K, rho, m, control, seed)
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 100, 0.5, m, False, 10))
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 100, 0.9, m, False, 10))
+# print(Arith_Put_Basket(S, S, 0.1, 0.3, r, T, 100, 0.5, m, False, 10))
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 80, 0.5, m, False, 10))
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 120, 0.5, m, False, 10))
+# print(Arith_Put_Basket(S, S, 0.5, 0.5, r, T, 100, 0.5, m, False, 10))
+
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 100, 0.5, m, True, 10))
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 100, 0.9, m, True, 10))
+# print(Arith_Put_Basket(S, S, 0.1, 0.3, r, T, 100, 0.5, m, True, 10))
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 80, 0.5, m, True, 10))
+# print(Arith_Put_Basket(S, S, 0.3, 0.3, r, T, 120, 0.5, m, True, 10))
+# print(Arith_Put_Basket(S, S, 0.5, 0.5, r, T, 100, 0.5, m, True, 10))
---
+++ option_pricer/closed_form_formulas.py
@@ -0,0 +1,154 @@
+# Implemented:
+# 1 closed-form formulas for geometric Asian call/put options
+# 2 closed-from formulas for geometric basket call/put options
+
+import numpy as np
+from scipy.stats import norm
+
+def geom_asian_call_option(s_t, sigma, r, T, K, n, t=0):
+    """Calculate the value of geometric Asian call option. By default 
+    calculate the value at time 0.
+
+    Args:
+        s_t: The value of asset S at time t
+        sigma: The volatility
+        r: The risk-free rate
+        T: The time to maturity(in years)
+        K: Strike
+        n: The number of observation times for geometric average n
+        t: Time t for option value, default 0
+
+    Returns:
+        The geometric Asian call option value at time t
+    """
+    _T = T-t
+
+    _sigma_hat_sq_T = sigma*sigma*(n+1)*(2*n+1)/(6*n*n)*_T # (sigma^2)*T, convience for latter calculation
+    _mu_hat_T = (r-0.5*sigma*sigma)*(n+1)/(2*n)*_T + 0.5*_sigma_hat_sq_T # mu*T, convience for latter calculation
+
+    _d1 = (np.log(s_t/K) + _mu_hat_T + 0.5*_sigma_hat_sq_T) / (np.sqrt(_sigma_hat_sq_T))
+    _d2 = _d1 - np.sqrt(_sigma_hat_sq_T)
+
+    _N1 = norm.cdf(_d1)
+    _N2 = norm.cdf(_d2)
+
+    return np.exp(-r*_T)*(s_t*np.exp(_mu_hat_T)*_N1 - K*_N2)
+
+def geom_asian_put_option(s_t, sigma, r, T, K, n, t=0):
+    """Calculate the value of geometric Asian put option. By default 
+    calculate the value at time 0.
+
+    Args:
+        s_t: The value of asset S at time t
+        sigma: The volatility
+        r: The risk-free rate
+        T: The time to maturity(in years)
+        K: Strike
+        n: The number of observation times for geometric average n
+        t: Time t for option value, default 0
+
+    Returns:
+        The geometric Asian put option value at time t
+    """
+    _T = T-t
+
+    _sigma_hat_sq_T = sigma*sigma*(n+1)*(2*n+1)/(6*n*n)*_T # (sigma^2)*T, convience for latter calculation
+    _mu_hat_T = (r-0.5*sigma*sigma)*(n+1)/(2*n)*_T + 0.5*_sigma_hat_sq_T # mu*T, convience for latter calculation
+
+    _d1 = (np.log(s_t/K) + _mu_hat_T + 0.5*_sigma_hat_sq_T) / (np.sqrt(_sigma_hat_sq_T))
+    _d2 = _d1 - np.sqrt(_sigma_hat_sq_T)
+
+    _N1 = norm.cdf(-_d1)
+    _N2 = norm.cdf(-_d2)
+
+    return np.exp(-r*_T)*(K*_N2 - s_t*np.exp(_mu_hat_T)*_N1)
+
+def geom_basket_call_option(s_t_1, s_t_2, sigma_1, sigma_2, r, T, K, rho, t=0):
+    """Calculate the value of geometric basket call option. By default 
+    calculate the value at time 0.
+
+    Args:
+        s_t_1: The value of asset S1 at time t
+        s_t_2: The value of asset S2 at time t
+        sigma_1: The volatility of S1
+        sigma_2: The volatility of S2
+        r: The risk-free rate
+        T: The time to maturity(in years)
+        K: Strike
+        rho: The correlation rho
+        t: Time t for option value, default 0
+
+    Returns:
+        The geometric basket call option value at time t
+    """
+    _T = T-t
+
+    _sigma_sq_T = sigma_1*sigma_2*rho/(2*2)*_T # (sigma^2)*T, convience for latter calculation
+    _mu_T = r*_T - 0.5*(sigma_1*sigma_1+sigma_2*sigma_2)/2*_T + 0.5*_sigma_sq_T/_T
+
+    _B_t = np.sqrt(s_t_1*s_t_2)
+
+    _d1 = (np.log(_B_t/K) + _mu_T + 0.5*_sigma_sq_T) / (np.sqrt(_sigma_sq_T))
+    _d2 = _d1 - np.sqrt(_sigma_sq_T)
+
+    _N1 = norm.cdf(_d1)
+    _N2 = norm.cdf(_d2)
+
+    return np.exp(-r*_T)*(_B_t*np.exp(_mu_T)*_N1 - K*_N2)
+
+def geom_basket_put_option(s_t_1, s_t_2, sigma_1, sigma_2, r, T, K, rho, t=0):
+    """Calculate the value of geometric basket call option. By default 
+    calculate the value at time 0.
+
+    Args:
+        s_t_1: The original value of asset S1
+        s_t_2: The original value of asset S2
+        sigma_1: The volatility of S1
+        sigma_2: The volatility of S2
+        r: The risk-free rate
+        T: The time to maturity(in years)
+        K: Strike
+        rho: The correlation rho
+        t: Time t for option value, default 0
+
+    Returns:
+        The geometric basket call option value at time t
+    """
+    _T = T-t
+
+    _sigma_sq_T = sigma_1*sigma_2*rho/(2*2)*_T # (sigma^2)*T, convience for latter calculation
+    _mu_T = r*_T - 0.5*(sigma_1*sigma_1+sigma_2*sigma_2)/2*_T + 0.5*_sigma_sq_T/_T
+
+    _B_t = np.sqrt(s_t_1*s_t_2)
+
+    _d1 = (np.log(_B_t/K) + _mu_T + 0.5*_sigma_sq_T) / (np.sqrt(_sigma_sq_T))
+    _d2 = _d1 - np.sqrt(_sigma_sq_T)
+
+    _N1 = norm.cdf(-_d1)
+    _N2 = norm.cdf(-_d2)
+
+    return np.exp(-r*_T)*(K*_N2 - _B_t*np.exp(_mu_T)*_N1)
+
+def main():
+    s_t = 100
+    sigma = 0.3
+    r = 0.05
+    T = 3
+    K = 100
+    n = 50
+
+    print('Asian call: ' + str(geom_asian_call_option(s_t, sigma, r, T, K, n)))
+    print('Asian put: ' + str(geom_asian_put_option(s_t, sigma, r, T, K, n)))
+
+    s_t_1 = 100
+    s_t_2 = 100
+    sigma_1 = 0.3
+    sigma_2 = 0.3
+    K = 100
+    rho = 0.5
+    
+    print('Basket call: ' + str(geom_basket_call_option(s_t_1, s_t_2, sigma_1, sigma_2, r, T, K, rho)))
+    print('Basket put: ' + str(geom_basket_put_option(s_t_1, s_t_2, sigma_1, sigma_2, r, T, K, rho)))
+
+if __name__ == '__main__':
+    main()
